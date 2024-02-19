5edcf144f2dc3f16b11b5cb8f42caf9fae46153c
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-02-19 15:46:30 +0200

    Am adaugat validare

--- src/NumarulDeModuri2.java
+++ src/NumarulDeModuri2.java
@@ -5,8 +5,10 @@ public class NumarulDeModuri2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduceti numarul de copii: ");
         int numarCopii = scanner.nextInt();
+        validareInput(numarCopii);
         System.out.println("Introduceti numarul de jucarii: ");
         int numarJucarii = scanner.nextInt();
+        validareInput(numarJucarii);
         int numarulDeModuri = calcululNumaruluiDeModuri(numarCopii, numarJucarii);
         System.out.println("Copiii isi pot alege cite o jucarie in " + numarulDeModuri + " moduri!");
     }
@@ -18,5 +20,12 @@ public class NumarulDeModuri2 {
             numarModuri *= optiuniCopii;
         }
         return numarModuri;
+    }
+
+    public static void validareInput(int input) {
+        if (input < 0) {
+            System.out.println("Numarul introdus trebuie sa fie pozitiv si diferit de zero.");
+            System.exit(1);
+        }
     }
 }
