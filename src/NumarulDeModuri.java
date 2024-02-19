601336d59bbc1431a8f3b49ea72bf20c33f4d705
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-02-19 15:07:36 +0200

    Update NumarulDeModuri.java

--- src/NumarulDeModuri.java
+++ src/NumarulDeModuri.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class NumarulDeModuri {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduceti numarul de copii n: ");
+        System.out.println("Introduceti numarul de copii (n): ");
         int n = scanner.nextInt();
-        System.out.println("Introduceti numarul de jucarii m: ");
+        System.out.println("Introduceti numarul de jucarii (m): ");
         int m = scanner.nextInt();
         int numarulModuri = 1;
-        for (int i = 0; i < m; i++) {
-            numarulModuri *= n;
+        for (int i = 0; i < n; i++) {
+            int optiunCopil = m - i;
+            numarulModuri *= optiunCopil;
         }
-        System.out.println("Numarul de moduri este : " + numarulModuri);
+        System.out.println("Copiii isi pot alege cite o jucarie in " + numarulModuri + " moduri!");
     }
 }
