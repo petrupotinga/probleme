5d66e088f1690d4cefedf6d72b73c2b2ef5f7c74
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-02-23 16:38:09 +0200

    Update NumeModificat.java

--- src/NumeModificat.java
+++ src/NumeModificat.java
@@ -1,15 +1,16 @@
 public class NumeModificat {
     public static void main(String[] args) {
         char[] id = new char[50];
-        String nume = "David Popovici";
-        char[] numeConvertit = nume.toCharArray();
+//        String nume = "David Popovici";
+//        char[] numeConvertit = nume.toCharArray();
+        char[]numeConvertit = {'D','a','v','i','d','/','P','o','p','o','v','i','c','i'};
         fNume(numeConvertit, id);
         System.out.println(id);
     }
 
     static void fNume(char[] s, char[] id) {
         String numeIntreg = new String(s);
-        String[] numeDespartit = numeIntreg.split(" ");
+        String[] numeDespartit = numeIntreg.split("/");
         String nume = numeDespartit[1] + 2024;
         char[] numemodificat = nume.toCharArray();
         System.arraycopy(numemodificat, 0, id, 0, numemodificat.length);
