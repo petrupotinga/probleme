ab7dcf694297b9f341a1e5b3057ae79af63a9ae3
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-03-12 20:29:14 +0200

    Update Exponent.java

--- src/Exponent.java
+++ src/Exponent.java
@@ -6,7 +6,12 @@ public class Exponent {
         System.out.println("Introduceti numarul n: ");
         int n = scanner.nextInt();
         int e = 0;
-        while (Math.pow(2, e) <= n) {
+//        while (Math.pow(2, e) <= n) {
+//            e++;
+//        }
+        int p2 = 1;
+        while (p2 <= n) {
+            p2 *= 2;
             e++;
         }
         System.out.println("Exponentul maxim e este: " + (e - 1));
