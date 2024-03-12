a60118881691df9174e5c21942ad543020520710
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-03-12 20:20:20 +0200

    Update CalculFactorial.java

--- src/CalculFactorial.java
+++ src/CalculFactorial.java
@@ -4,15 +4,15 @@ public class CalculFactorial {
         long factorial = factorial(n);
         System.out.println("Factorialul lui " + n + " este: " + factorial);
     }
-
     public static long factorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Factorialul nu este definit pentru numere negative.");
         }
-        long factorial = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial *= i;
+        long result = 1;
+        while (n > 0) {
+            result *= n;
+            n--;
         }
-        return factorial;
+        return result;
     }
 }
