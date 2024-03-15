af4b090062944161b0db3a9585783ac5c6a232b9
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-03-15 16:01:51 +0200

    Update DublarePrimaCifra2.java

--- src/DublarePrimaCifra2.java
+++ src/DublarePrimaCifra2.java
@@ -1,26 +1,24 @@
 public class DublarePrimaCifra2 {
     public static void main(String[] args) {
-        int num = 1645;
-        int primaCifra = extragePrimaCifra(num);
-        int count = cifre(num);
-        int result = (int) Math.pow(10, count) * primaCifra + num;
-        System.out.println("Numarul initial este: " + num);
+        int numar = 2548;
+        int primaCifra = extragePrimaCifra(numar);
+        int count = cifre(numar);
+        int result = (int) Math.pow(10, count) * primaCifra + numar;
+        System.out.println("Numarul initial este: " + numar);
         System.out.println("Rezultatul este: " + result);
     }
 
     public static int extragePrimaCifra(int numar) {
-        while (numar >= 10) {
+        while (numar >= 10 || numar <= -10) {
             numar /= 10;
         }
         return numar;
     }
 
     public static int cifre(int numar) {
-        int numarCifre = 0;
-        while (numar >= 10) {
-            numar /= 10;
-            numarCifre++;
+        if (numar == 0) {
+            return 1;
         }
-        return numarCifre + 1;
+        return (int) Math.log10(Math.abs(numar)) + 1;
     }
 }
